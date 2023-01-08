201c9404a58fe1a1a6c7fc7d351edea4fc335866
katsuro666 <kreepinoutdakut@gmail.com>
2023-01-08 16:27:26 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let abs = Math.abs(n)
+    let number = abs.toString(10)
+    let result = ''
+    for (let i = number.length - 1; i >= 0; i--) {
+        result = `${result}${number[i]}`
+  }
+  return result
 }
